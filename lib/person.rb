029d4ad8b395fdbaba1d8c1ddc794b176be63aac
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-09 15:09:11 +0300

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+            puts "The Person is walking"
+    end
+end
+
+kel = Person.new
+kel.talk
+
+kel = Person.new
+kel.walk
